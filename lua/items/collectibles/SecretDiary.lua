57852210b264515d2aa2377d7c165b50e1efc86a
BenevolusGoat <brude602@gmail.com>
2025-03-21 19:21:56 -0400

    Adjust how Secret Diary wisp is removed

--- lua/items/collectibles/SecretDiary.lua
+++ lua/items/collectibles/SecretDiary.lua
@@ -1,18 +1,24 @@
 local mod = Furtherance
 
 function mod:UseSecretDiary(_, _, player)
-    local BirthrightWisp = player:AddItemWisp(CollectibleType.COLLECTIBLE_BIRTHRIGHT, Vector(999999, 999999), false)
-    BirthrightWisp.Target = player
-    return true
+	player:AddItemWisp(CollectibleType.COLLECTIBLE_BIRTHRIGHT, Vector(999999, 999999), false)
+	return true
 end
+
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseSecretDiary, CollectibleType.COLLECTIBLE_SECRET_DIARY)
 
-function mod:RemoveWisp()
-    for _, wisp in ipairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, FamiliarVariant.ITEM_WISP)) do
-        if wisp.Target ~= nil then
-            wisp:Remove()
-            wisp:Kill()
-        end
-    end
+function mod:RemoveWisp(player)
+	--IsPaused() returns true on update callbacks right before doing a room transition
+	--Unfortunately TemporaryEffects are still cleared when this happens. I'll probably wanna use HiddenItemManager for this later anyways
+	if Game():IsPaused() then
+		for _, wisp in ipairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, FamiliarVariant.ITEM_WISP, CollectibleType.COLLECTIBLE_BIRTHRIGHT)) do
+			if GetPtrHash(player) == GetPtrHash(wisp:ToFamiliar().Player) then
+				wisp:Remove()
+				wisp:Kill()
+				break
+			end
+		end
+	end
 end
-mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.RemoveWisp)
+
+mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.RemoveWisp)
