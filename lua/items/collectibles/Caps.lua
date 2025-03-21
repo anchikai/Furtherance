9b2648c086663a86c01508ebeef915431efcd1ab
BenevolusGoat <brude602@gmail.com>
2025-03-21 14:28:08 -0400

    Work on Ceres to get tentacles to spawn as intended. Method of initiating spawn works, but having a working Worm Friend will need to be left to a custom entity.

--- lua/items/collectibles/Caps.lua
+++ lua/items/collectibles/Caps.lua
@@ -1,7 +1,8 @@
 local mod = Furtherance
 
+---@param player EntityPlayer
 function mod:UseCaps(_, _, player, slot, data)
-	player:UseCard(Card.CARD_HUGE_GROWTH, 0)
+	player:UseCard(Card.CARD_HUGE_GROWTH, UseFlag.USE_NOANIM)
 	SFXManager():Stop(SoundEffect.SOUND_HUGE_GROWTH)
 	return true
 end
