2c03a5e1eb0c2929628ca27cead20d2a20dc7d9c
BenevolusGoat <brude602@gmail.com>
2025-03-21 20:14:35 -0400

    Holy Heart should check if player can pick up soul hearts

--- lua/items/trinkets/HolyHeart.lua
+++ lua/items/trinkets/HolyHeart.lua
@@ -1,29 +1,35 @@
 local mod = Furtherance
 local rng = RNG()
 
+---@param collider Entity
 function mod:CollectHeart(pickup, collider)
-	if collider:ToPlayer() then
-		local player = collider:ToPlayer()
-		if player:HasTrinket(TrinketType.TRINKET_HOLY_HEART, false) then
-			if pickup.SubType == HeartSubType.HEART_ETERNAL then
-				if rng:RandomInt(3) == 1 then
-					player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER)
-				end
-			elseif pickup.SubType == HeartSubType.HEART_SOUL then
-				if rng:RandomInt(20) == 1 then
-					player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER)
-				end
-			elseif pickup.SubType == HeartSubType.HEART_BLENDED then
-				if rng:RandomInt(20) == 1 then
-					player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER)
-				end
-			elseif pickup.SubType == HeartSubType.HEART_HALF_SOUL then
-				if rng:RandomInt(50) == 1 then
-					player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER)
-				end
+	local player = collider:ToPlayer()
+	if player
+		and player:HasTrinket(TrinketType.TRINKET_HOLY_HEART, false)
+		and (player:CanPickSoulHearts()
+		or pickup.SubType == HeartSubType.HEART_ETERNAL
+		or (pickup.SubType == HeartSubType.HEART_BLENDED
+			and player:CanPickRedHearts())
+		)
+	then
+		if pickup.SubType == HeartSubType.HEART_ETERNAL then
+			if rng:RandomInt(3) == 1 then
+				player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER)
+			end
+		elseif pickup.SubType == HeartSubType.HEART_SOUL then
+			if rng:RandomInt(20) == 1 then
+				player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER)
+			end
+		elseif pickup.SubType == HeartSubType.HEART_BLENDED then
+			if rng:RandomInt(20) == 1 then
+				player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER)
+			end
+		elseif pickup.SubType == HeartSubType.HEART_HALF_SOUL then
+			if rng:RandomInt(50) == 1 then
+				player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER)
 			end
 		end
 	end
 end
 
-mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, mod.CollectHeart, PickupVariant.PICKUP_HEART)
+mod:AddPriorityCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, CallbackPriority.LATE, mod.CollectHeart, PickupVariant.PICKUP_HEART)
