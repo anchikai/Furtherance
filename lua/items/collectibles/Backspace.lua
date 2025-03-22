da47a335e6c217d9f83775aaed3a75bada3c39c8
BenevolusGoat <brude602@gmail.com>
2025-03-22 15:33:14 -0400

    Backspace can open any (entered) door now, including wave-based rooms.

--- lua/items/collectibles/Backspace.lua
+++ lua/items/collectibles/Backspace.lua
@@ -1,10 +1,24 @@
 local mod = Furtherance
 local game = Game()
+local sfx = SFXManager()
 
 function mod:UseBackSpace(_, _, player)
 	local level = game:GetLevel()
-	SFXManager():Play(SoundEffect.SOUND_GOLDENKEY)
-	game:GetRoom():GetDoor(level.EnterDoor):Open()
+	local door = game:GetRoom():GetDoor(level.EnterDoor)
+	if door and not door:IsOpen() then
+		game:GetRoom():GetDoor(level.EnterDoor):Open()
+		if not door:IsOpen() then
+			door:TryUnlock(player, true)
+		end
+		if door:IsOpen() then
+			sfx:Play(SoundEffect.SOUND_GOLDENKEY)
+		end
+	elseif door and door:IsOpen() then
+		sfx:Play(SoundEffect.SOUND_BOSS2INTRO_ERRORBUZZ)
+	end
+	if not door or not door:IsOpen() then
+		sfx:Play(SoundEffect.SOUND_BOSS2INTRO_ERRORBUZZ)
+	end
 	return true
 end
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseBackSpace, CollectibleType.COLLECTIBLE_BACKSPACE_KEY)
