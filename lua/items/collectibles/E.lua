37c66be0167b393eaeefb660b5c1e4a5a32f3ed1
BenevolusGoat <brude602@gmail.com>
2025-03-23 11:31:21 -0400

    E Key's Giga Bombs now inherint player's BombFlags

--- lua/items/collectibles/E.lua
+++ lua/items/collectibles/E.lua
@@ -1,7 +1,10 @@
 local mod = Furtherance
 
+---@param player EntityPlayer
 function mod:UseE(_, _, player)
-	Isaac.Spawn(EntityType.ENTITY_BOMBDROP, BombVariant.BOMB_GIGA, 0, player.Position, Vector.Zero, player)
+	local bomb = Isaac.Spawn(EntityType.ENTITY_BOMB, BombVariant.BOMB_GIGA, 0, player.Position, Vector.Zero, player):ToBomb()
+	---@cast bomb EntityBomb
+	bomb:AddTearFlags(player:GetBombFlags())
 	return true
 end
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseE, CollectibleType.COLLECTIBLE_E_KEY)
