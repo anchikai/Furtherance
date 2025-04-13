8080559769c6af804f021ddb94a3f092bd3448f4
BenevolusGoat <brude602@gmail.com>
2025-04-13 15:20:52 -0400

    Exsanguination

--- scripts/furtherance/unlocks/unlocks_leah/exsanguination.lua
+++ scripts/furtherance/unlocks/unlocks_leah/exsanguination.lua
@@ -6,8 +6,10 @@ Furtherance.Item.EXSANGUINATION = EXSANGUINATION
 
 EXSANGUINATION.ID = Isaac.GetItemIdByName("Exsanguination")
 
-EXSANGUINATION.DAMAGE_MULT = 0.1
-EXSANGUINATION.HEART_REMOVAL_CHANCE = 0.5
+EXSANGUINATION.DAMAGE_MULT_UP = 0.1
+EXSANGUINATION.HEART_REMOVAL_CHANCE = 0.05
+
+local sqrt = math.sqrt
 
 ---@param heart EntityPickup
 ---@param collider Entity
@@ -24,40 +26,29 @@ end
 
 Mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, EXSANGUINATION.PickupHeart, PickupVariant.PICKUP_HEART)
 
+---@param player EntityPlayer
 function EXSANGUINATION:DamageUp(player)
 	if player:HasCollectible(EXSANGUINATION.ID) then
-		player.Damage = player.Damage + (player:GetEffects():GetCollectibleEffectNum(EXSANGUINATION.ID) * EXSANGUINATION.DAMAGE_MULT)
+		player.Damage = player.Damage + (player:GetEffects():GetCollectibleEffectNum(EXSANGUINATION.ID) * EXSANGUINATION.DAMAGE_MULT_UP)
 	end
 end
 
 Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, EXSANGUINATION.DamageUp, CacheFlag.CACHE_DAMAGE)
 
----@param entType EntityType
----@param variant PickupVariant
----@param spawner Entity
----@param seed integer
-function EXSANGUINATION:PreHeartSpawn(entType, variant, _, _, _, spawner, seed)
-	if entType == EntityType.ENTITY_PICKUP
-		and variant == PickupVariant.PICKUP_HEART
-		and PlayerManager.AnyoneHasCollectible(EXSANGUINATION.ID)
+---@param pickup EntityPickup
+function EXSANGUINATION:OnHeartInit(pickup)
+	if PlayerManager.AnyoneHasCollectible(EXSANGUINATION.ID)
+		and pickup.FrameCount == 1
+		--Making sure its not already disappearing
+		and pickup.Timeout == -1
+		and pickup:GetSprite():IsPlaying("Appear")
+		and not (pickup.SpawnerEntity and pickup.SpawnerEntity:ToPlayer())
 	then
-		local floor_save = Mod:FloorSave()
-		local key = tostring(seed)
-		floor_save.ExsanguinationRoll = floor_save.ExsanguinationRoll or {}
-		if floor_save.ExsanguinationRoll[key] then
-			return
-		end
-		if spawner and spawner:ToPlayer() then
-			floor_save.ExsanguinationRoll[key] = true
-			return
-		end
-		local rng = RNG(seed)
+		local rng = PlayerManager.FirstCollectibleOwner(EXSANGUINATION.ID):GetCollectibleRNG(EXSANGUINATION.ID)
 		if rng:RandomFloat() <= EXSANGUINATION.HEART_REMOVAL_CHANCE then
-			return {1000, Mod.REPLACER_EFFECT, 0, seed}
-		else
-			floor_save.ExsanguinationRoll[key] = true
+			pickup.Timeout = 60
 		end
 	end
 end
 
-Mod:AddPriorityCallback(ModCallbacks.MC_PRE_ENTITY_SPAWN, CallbackPriority.IMPORTANT, EXSANGUINATION.PreHeartSpawn)
+Mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, EXSANGUINATION.OnHeartInit, PickupVariant.PICKUP_HEART)
