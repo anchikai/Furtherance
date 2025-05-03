f6e744223446f9c75ddb9cf53d9c8c406bc56c96
BenevolusGoat <brude602@gmail.com>
2025-05-02 22:02:07 -0400

    Adjust code for Secret Diary to use AddInnateCollectible instead of innate callback, which is to be deprecated

--- scripts/furtherance/unlocks/unlocks_leah/secret_diary.lua
+++ scripts/furtherance/unlocks/unlocks_leah/secret_diary.lua
@@ -6,19 +6,32 @@ Furtherance.Item.SECRET_DIARY = SECRET_DIARY
 
 SECRET_DIARY.ID = Isaac.GetItemIdByName("Secret Diary")
 
-function SECRET_DIARY:UseSecretDiary()
+---@param player EntityPlayer
+function SECRET_DIARY:UseSecretDiary(_, _, player)
+	player:AddInnateCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT)
 	return true
 end
 
 Mod:AddCallback(ModCallbacks.MC_USE_ITEM, SECRET_DIARY.UseSecretDiary, SECRET_DIARY.ID)
 
-function SECRET_DIARY:Test(modCount, player, itemID, onlyTrue)
-	if itemID == CollectibleType.COLLECTIBLE_BIRTHRIGHT
-		and player:GetEffects():HasCollectibleEffect(SECRET_DIARY.ID)
-		and not onlyTrue
+---@param player EntityPlayer
+---@param itemConfigItem ItemConfigItem
+function SECRET_DIARY:RemoveBirthrightOnNewRoom(player, itemConfigItem)
+	if itemConfigItem:IsCollectible()
+		and itemConfigItem.ID == SECRET_DIARY.ID
 	then
-		return modCount + 1
+		player:AddInnateCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT, -1)
 	end
 end
 
-Mod:AddCallback(ModCallbacks.MC_PRE_PLAYER_APPLY_INNATE_COLLECTIBLE_NUM, SECRET_DIARY.Test)
+Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_TRIGGER_EFFECT_REMOVED, SECRET_DIARY.RemoveBirthrightOnNewRoom)
+
+function SECRET_DIARY:RemoveBirthrightOnGameExit()
+	Mod.Foreach.Player(function (player, index)
+		if player:GetEffects():HasCollectibleEffect(SECRET_DIARY.ID) then
+			player:AddInnateCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT, -1)
+		end
+	end)
+end
+
+Mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, SECRET_DIARY.RemoveBirthrightOnGameExit)
