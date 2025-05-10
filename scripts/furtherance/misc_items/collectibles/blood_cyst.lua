764602664dd7d6719c166f1c9ac2ba275c6d33a2
BenevolusGoat <brude602@gmail.com>
2025-05-09 20:23:02 -0400

    Blood Cyst test

--- scripts/furtherance/misc_items/collectibles/blood_cyst.lua
+++ scripts/furtherance/misc_items/collectibles/blood_cyst.lua
@@ -7,26 +7,8 @@ Furtherance.Item.BLOOD_CYST = BLOOD_CYST
 BLOOD_CYST.ID = Isaac.GetItemIdByName("Blood Cyst")
 BLOOD_CYST.FAMILIAR = Isaac.GetEntityVariantByName("Blood Cyst")
 
-function BLOOD_CYST:RespawnCyst()
-	local room = Mod.Room()
-	Mod.Foreach.Familiar(function(familiar, index)
-		familiar:Remove()
-	end, BLOOD_CYST.FAMILIAR, -1, { Inverse = true })
-
-	if room:IsClear() or not PlayerManager.AnyoneHasCollectible(BLOOD_CYST.ID) then return end
-
-	Mod.Foreach.Player(function(player)
-		for _ = 1, player:GetCollectibleNum(BLOOD_CYST.ID) do
-			local position = Isaac.GetFreeNearPosition(room:GetRandomPosition(0), 40)
-			Isaac.Spawn(EntityType.ENTITY_FAMILIAR, BLOOD_CYST.FAMILIAR, 0, position, Vector.Zero, player)
-		end
-	end)
-end
-
-Mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, BLOOD_CYST.RespawnCyst)
-
 ---@param familiar EntityFamiliar
-function BLOOD_CYST:OnFamiliarInit(familiar)
+function BLOOD_CYST:GrantHitsphere(familiar)
 	local hitbox = Isaac.Spawn(EntityType.ENTITY_BOIL, BLOOD_CYST.FAMILIAR, 0, familiar.Position, Vector.Zero, familiar):ToNPC()
 	---@cast hitbox EntityNPC
 	hitbox.HitPoints = 0
@@ -39,7 +21,28 @@ function BLOOD_CYST:OnFamiliarInit(familiar)
 	hitbox:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
 end
 
-Mod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, BLOOD_CYST.OnFamiliarInit, BLOOD_CYST.FAMILIAR)
+---@param familiar EntityFamiliar
+function BLOOD_CYST:UpdateCystState(familiar)
+	local room = Mod.Room()
+	if room:IsClear() then
+		familiar.State = 1
+		familiar.Visible = false
+	else
+		familiar.State = 0
+		familiar.Visible = true
+		local position = Isaac.GetFreeNearPosition(room:GetRandomPosition(0), 40)
+		familiar.Position = position
+		BLOOD_CYST:GrantHitsphere(familiar)
+	end
+end
+
+function BLOOD_CYST:RespawnCyst()
+	Mod.Foreach.Familiar(function(familiar, index)
+		BLOOD_CYST:UpdateCystState(familiar)
+	end, BLOOD_CYST.FAMILIAR)
+end
+
+Mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, BLOOD_CYST.RespawnCyst)
 
 ---@param npc EntityNPC
 function BLOOD_CYST:StopBoilUpdate(npc)
@@ -56,12 +59,10 @@ Mod:AddPriorityCallback(ModCallbacks.MC_PRE_NPC_UPDATE, CallbackPriority.IMPORTA
 ---@param npc EntityNPC
 function BLOOD_CYST:KillFamiliar(npc)
 	if npc.Variant == BLOOD_CYST.FAMILIAR then
-		if npc.SpawnerEntity
-			and npc.SpawnerEntity:ToFamiliar()
-			and npc.SpawnerVariant == BLOOD_CYST.FAMILIAR
-		then
+		local familiar = npc.SpawnerEntity and npc.SpawnerEntity:ToFamiliar()
+		if familiar and familiar == BLOOD_CYST.FAMILIAR then
 			npc:Remove()
-			npc.SpawnerEntity:Kill()
+			BLOOD_CYST:OnDeath(familiar)
 		end
 		return false
 	end
@@ -69,10 +70,8 @@ end
 
 Mod:AddPriorityCallback(ModCallbacks.MC_POST_ENTITY_KILL, CallbackPriority.IMPORTANT, BLOOD_CYST.KillFamiliar, EntityType.ENTITY_BOIL)
 
----@param ent Entity
-function BLOOD_CYST:OnDeath(ent)
-	if ent.Variant ~= BLOOD_CYST.FAMILIAR then return end
-	local familiar = ent:ToFamiliar()
+---@param familiar EntityFamiliar
+function BLOOD_CYST:OnDeath(familiar)
 	---@cast familiar EntityFamiliar
 	for _ = 1, 20 do
 		local tear = Isaac.Spawn(EntityType.ENTITY_TEAR, TearVariant.BLOOD, 0, familiar.Position,
@@ -84,6 +83,19 @@ function BLOOD_CYST:OnDeath(ent)
 		tear:ResetSpriteScale(true)
 	end
 	Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.BLOOD_EXPLOSION, 0, familiar.Position, Vector.Zero, familiar)
+	familiar.State = 1
+	familiar.Visible = false
 end
 
-Mod:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, BLOOD_CYST.OnDeath, EntityType.ENTITY_FAMILIAR)
+function BLOOD_CYST:FamiliarCache(player)
+	local effects = player:GetEffects()
+	local numFamiliars = player:GetCollectibleNum(BLOOD_CYST.ID) + effects:GetCollectibleEffectNum(BLOOD_CYST.ID)
+	local rng = player:GetCollectibleRNG(BLOOD_CYST.ID)
+	rng:Next()
+	local familiars = player:CheckFamiliarEx(BLOOD_CYST.FAMILIAR, numFamiliars, rng, Mod.ItemConfig:GetCollectible(BLOOD_CYST.ID))
+	for _, familiar in ipairs(familiars) do
+		BLOOD_CYST:UpdateCystState(familiar)
+	end
+end
+
+Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, BLOOD_CYST.FamiliarCache, CacheFlag.CACHE_FAMILIARS)
