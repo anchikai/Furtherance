4fef32935987cb03e2654dc799856543f584128e
BenevolusGoat <brude602@gmail.com>
2025-05-25 12:39:47 -0400

    Severed Ear and Golden Port costumes

--- scripts/furtherance/unlocks/unlocks_peter/severed_ear.lua
+++ scripts/furtherance/unlocks/unlocks_peter/severed_ear.lua
@@ -5,6 +5,7 @@ local SEVERED_EAR = {}
 Furtherance.Item.SEVERED_EAR = SEVERED_EAR
 
 SEVERED_EAR.ID = Isaac.GetItemIdByName("Severed Ear")
+SEVERED_EAR.DOUBLE_COSTUME = Isaac.GetCostumeIdByPath("gfx/characters/n_double_severed_ear.anm2")
 
 SEVERED_EAR.DAMAGE_MULT_UP = 1.2
 SEVERED_EAR.FIRE_DELAY_DIV_DOWN = 0.8
@@ -32,3 +33,19 @@ function SEVERED_EAR:GetSeveredEar(player, flag)
 end
 
 Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, SEVERED_EAR.GetSeveredEar)
+
+---@param player EntityPlayer
+---@param itemID CollectibleType
+function SEVERED_EAR:UpdateDoubleCostume(player, itemID)
+	local numEars = player:GetCollectibleNum(itemID)
+	if numEars >= 2 then
+		player:AddNullCostume(SEVERED_EAR.DOUBLE_COSTUME)
+	else
+		player:TryRemoveNullCostume(SEVERED_EAR.DOUBLE_COSTUME)
+	end
+end
+
+Mod:AddCallback(ModCallbacks.MC_POST_ADD_COLLECTIBLE, function(_, itemID, _, _, _, _, player)
+	SEVERED_EAR:UpdateDoubleCostume(player, itemID)
+end, SEVERED_EAR.ID)
+Mod:AddCallback(ModCallbacks.MC_POST_TRIGGER_COLLECTIBLE_REMOVED, SEVERED_EAR.UpdateDoubleCostume, SEVERED_EAR.ID)
