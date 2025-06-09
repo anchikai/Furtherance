20c081096b84841843244c31d40f122bafac297f
BenevolusGoat <brude602@gmail.com>
2025-06-09 11:07:51 -0400

    Mandrake and new Polydipsia costumes

--- scripts/furtherance/characters/miriam/miriam.lua
+++ scripts/furtherance/characters/miriam/miriam.lua
@@ -23,5 +23,18 @@ end
 
 Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MIRIAM.OnPlayerInit, PlayerVariant.PLAYER)
 
+---@param player EntityPlayer
+---@param itemConfig ItemConfigItem
+function MIRIAM:IgnorePolydipsiaCostume(itemConfig, player)
+	if itemConfig.ID == Mod.Item.POLYDIPSIA.ID
+		and itemConfig:IsCollectible()
+		and MIRIAM:IsMiriam(player)
+	then
+		return true
+	end
+end
+
+Mod:AddCallback(ModCallbacks.MC_PRE_PLAYER_ADD_COSTUME, MIRIAM.IgnorePolydipsiaCostume)
+
 Mod.Include("scripts.furtherance.characters.miriam.whirlpool")
 Mod.Include("scripts.furtherance.characters.miriam.tambourine")
