68bf6ff239d9c9afa0e6a1a849436d27e24a24ba
BenevolusGoat <brude602@gmail.com>
2025-06-29 15:36:04 -0400

    Liberation costume is removed on effect activation and re-added after

--- scripts/furtherance/unlocks/unlocks_peter/liberation.lua
+++ scripts/furtherance/unlocks/unlocks_peter/liberation.lua
@@ -20,6 +20,11 @@ function LIBERATION:TryActivateLiberation(ent)
 			player:UseActiveItem(CollectibleType.COLLECTIBLE_DADS_KEY, false, false, true, false, -1)
 			Mod.Foreach.Player(function(_player)
 				if _player:HasCollectible(LIBERATION.ID) then
+					local liberationItemConfig = Mod.ItemConfig:GetCollectible(LIBERATION.ID)
+					if player:IsItemCostumeVisible(liberationItemConfig, PlayerSpriteLayer.SPRITE_HEAD1) then
+						player:RemoveCostume(liberationItemConfig)
+						Mod:GetData(player).LiberationGranted = true
+					end
 					_player:AddCacheFlags(CacheFlag.CACHE_FLYING, true)
 					_player:AddCollectibleEffect(CollectibleType.COLLECTIBLE_BIBLE, true)
 				end
@@ -29,3 +34,27 @@ function LIBERATION:TryActivateLiberation(ent)
 end
 
 Mod:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, LIBERATION.TryActivateLiberation)
+
+---@param itemConfig ItemConfigItem
+function LIBERATION:ReaddCostume(itemConfig)
+	if itemConfig:IsCollectible() and itemConfig.ID == LIBERATION.ID then
+		Mod.Foreach.Player(function (player, index)
+			local data = Mod:GetData(player)
+			if player:HasCollectible(LIBERATION.ID) and data.LiberationGranted then
+				player:AddCostume(itemConfig)
+				data.LiberationGranted = nil
+			end
+		end)
+	end
+end
+
+Mod:AddCallback(ModCallbacks.MC_POST_ROOM_TRIGGER_EFFECT_REMOVED, LIBERATION.ReaddCostume)
+
+---@param shouldSave boolean
+function LIBERATION:ReaddCostumeOnExit(shouldSave)
+	if shouldSave then
+		LIBERATION:ReaddCostume(Mod.ItemConfig:GetCollectible(LIBERATION.ID))
+	end
+end
+
+Mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, LIBERATION.ReaddCostumeOnExit)
