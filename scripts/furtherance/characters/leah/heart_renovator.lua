ffcd3fbcd600d2935ae136c34621ae7aa6b8e1a8
BenevolusGoat <brude602@gmail.com>
2025-06-29 15:14:44 -0400

    Heart Renovator accounts for directly added health

--- scripts/furtherance/characters/leah/heart_renovator.lua
+++ scripts/furtherance/characters/leah/heart_renovator.lua
@@ -1,3 +1,5 @@
+--#region Variables
+
 local Mod = Furtherance
 
 local HEART_RENOVATOR = {}
@@ -14,23 +16,29 @@ local counter = Sprite("gfx/ui/hudpickups.anm2", true)
 counter:SetFrame("Idle", 15)
 HEART_RENOVATOR.CounterSprite = counter
 
+--#endregion
+
+--#region Helpers
+
 function HEART_RENOVATOR:GetMaxHeartCounter(player)
 	return HEART_RENOVATOR.MAX_COUNTER
 end
 
 ---@param player EntityPlayer
----@param flags UseFlag
-function HEART_RENOVATOR:OnUse(_, _, player, flags)
-	if player:GetBrokenHearts() > 0 then
-		Mod.SFXMan:Play(SoundEffect.SOUND_HEARTBEAT)
-		player:AddBrokenHearts(-1)
-		local player_run_save = Mod:RunSave(player)
-		player_run_save.HeartRenovatorDamage = (player_run_save.HeartRenovatorDamage or 0) + 1
-	end
-	return true
-end
+---@param amount integer
+---@param isBlended? boolean
+function HEART_RENOVATOR:GetOverflowAmount(player, amount, isBlended)
+	local maxHearts = player:GetEffectiveMaxHearts()
+	local emptyHealth = maxHearts - player:GetHearts() - (player:GetRottenHearts() * 2)
 
-Mod:AddCallback(ModCallbacks.MC_USE_ITEM, HEART_RENOVATOR.OnUse, HEART_RENOVATOR.ID)
+	if emptyHealth < amount
+		and (not isBlended or maxHearts + player:GetSoulHearts() == player:GetHeartLimit())
+	then
+		amount = amount - emptyHealth
+		return true, amount
+	end
+	return false, 0
+end
 
 ---Will determine if the heart collided with would normally be unable to be picked up or if the collected health would overflow
 ---
@@ -55,15 +63,7 @@ function HEART_RENOVATOR:CannotPickRedHeartsOrWillOverflow(pickup, player)
 	end
 
 	if canCollect then
-		local maxHearts = player:GetEffectiveMaxHearts()
-		local emptyHealth = maxHearts - player:GetHearts() - (player:GetRottenHearts() * 2)
-		if emptyHealth < amount
-			and emptyHealth >= 0
-			and (not isBlended or maxHearts + player:GetSoulHearts() == player:GetHeartLimit())
-		then
-			amount = amount - emptyHealth
-			canOverflow = true
-		end
+		canOverflow, amount = HEART_RENOVATOR:GetOverflowAmount(player, amount, isBlended)
 	end
 
 	if (not canCollect and Mod:PricedPickup(player, pickup)) or canOverflow then
@@ -74,9 +74,53 @@ function HEART_RENOVATOR:CannotPickRedHeartsOrWillOverflow(pickup, player)
 	end
 end
 
+--#endregion
+
+--#region On use
+
+---@param player EntityPlayer
+---@param flags UseFlag
+function HEART_RENOVATOR:OnUse(_, _, player, flags)
+	if player:GetBrokenHearts() > 0 then
+		Mod.SFXMan:Play(SoundEffect.SOUND_HEARTBEAT)
+		player:AddBrokenHearts(-1)
+		local player_run_save = Mod:RunSave(player)
+		player_run_save.HeartRenovatorDamage = (player_run_save.HeartRenovatorDamage or 0) + 1
+	end
+	return true
+end
+
+Mod:AddCallback(ModCallbacks.MC_USE_ITEM, HEART_RENOVATOR.OnUse, HEART_RENOVATOR.ID)
+
+---@param player EntityPlayer
+function HEART_RENOVATOR:DamageUp(player)
+	local player_run_save = Mod:RunSave(player)
+	if player_run_save.HeartRenovatorDamage and player:HasCollectible(HEART_RENOVATOR.ID) then
+		local addAmount = player_run_save.HeartRenovatorDamage * HEART_RENOVATOR.DAMAGE_MULT
+		if Mod.Character.LEAH:IsLeah(player) and player:HasCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT) then
+			addAmount = addAmount * 2
+		end
+		player.Damage = player.Damage + addAmount
+	end
+end
+
+Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, HEART_RENOVATOR.DamageUp, CacheFlag.CACHE_DAMAGE)
+
+--#endregion
+
+--#region Add to counter
+
+---@param player EntityPlayer
+---@param amount integer
+function HEART_RENOVATOR:AddToCounter(player, amount)
+	local player_run_save = Mod:RunSave(player)
+	local maxCount = HEART_RENOVATOR:GetMaxHeartCounter(player)
+	player_run_save.HeartRenovatorCounter = Mod:Clamp(player_run_save.HeartRenovatorCounter + amount, 0, maxCount)
+end
+
 ---@param pickup EntityPickup
 ---@param collider Entity
-function HEART_RENOVATOR:AddToHeartCounter(pickup, collider)
+function HEART_RENOVATOR:AddHeartPickupToCounter(pickup, collider)
 	local player = collider:ToPlayer()
 	if not (player
 			and player:HasCollectible(HEART_RENOVATOR.ID)
@@ -92,25 +136,11 @@ function HEART_RENOVATOR:AddToHeartCounter(pickup, collider)
 	local amount = HEART_RENOVATOR:CannotPickRedHeartsOrWillOverflow(pickup, player)
 
 	if amount then
-		player_run_save.HeartRenovatorCounter = Mod:Clamp(player_run_save.HeartRenovatorCounter + amount, 0, HEART_RENOVATOR:GetMaxHeartCounter(player))
+		HEART_RENOVATOR:AddToCounter(player, amount)
 	end
 end
 
-Mod:AddPriorityCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, CallbackPriority.EARLY, HEART_RENOVATOR.AddToHeartCounter, PickupVariant.PICKUP_HEART)
-
----@param player EntityPlayer
-function HEART_RENOVATOR:DamageUp(player)
-	local player_run_save = Mod:RunSave(player)
-	if player_run_save.HeartRenovatorDamage and player:HasCollectible(HEART_RENOVATOR.ID) then
-		local addAmount = player_run_save.HeartRenovatorDamage * HEART_RENOVATOR.DAMAGE_MULT
-		if Mod.Character.LEAH:IsLeah(player) and player:HasCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT) then
-			addAmount = addAmount * 2
-		end
-		player.Damage = player.Damage + addAmount
-	end
-end
-
-Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, HEART_RENOVATOR.DamageUp, CacheFlag.CACHE_DAMAGE)
+Mod:AddPriorityCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, CallbackPriority.EARLY, HEART_RENOVATOR.AddHeartPickupToCounter, PickupVariant.PICKUP_HEART)
 
 ---@param player EntityPlayer
 function HEART_RENOVATOR:ConsumeHeartCounter(player)
@@ -144,14 +174,19 @@ end
 
 Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, HEART_RENOVATOR.ConsumeHeartCounter)
 
-function HEART_RENOVATOR:TwoOfHearts(_, player)
-	if player:HasCollectible(HEART_RENOVATOR.ID) then
-		local player_run_save = Mod:RunSave(player)
-		player_run_save.HeartRenovatorCounter = player_run_save.HeartRenovatorCounter * 2
+---@param player EntityPlayer
+---@param amount integer
+function HEART_RENOVATOR:AddExtraRedHealth(player, amount)
+	if player:HasCollectible(HEART_RENOVATOR.ID) and amount > 0 then
+		local willOverflow, newAmount = HEART_RENOVATOR:GetOverflowAmount(player, amount, false)
+
+		if willOverflow then
+			HEART_RENOVATOR:AddToCounter(player, newAmount)
+		end
 	end
 end
 
-Mod:AddCallback(ModCallbacks.MC_USE_CARD, HEART_RENOVATOR.TwoOfHearts, Card.CARD_HEARTS_2)
+Mod:AddPriorityCallback(ModCallbacks.MC_PRE_PLAYER_ADD_HEARTS, CallbackPriority.LATE, HEART_RENOVATOR.AddExtraRedHealth, AddHealthType.RED)
 
 HudHelper.RegisterHUDElement({
 	Name = "Heart Renovator Counter",
