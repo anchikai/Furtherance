b642b53af0b60b18fae3fc9dae775160dd8787b2
BenevolusGoat <brude602@gmail.com>
2025-09-08 12:01:43 -0400

    Add DamageFlag.FLAG_NO_PENALTIES to manual use of Soul of Leah

--- scripts/furtherance/unlocks/unlocks_leah_b/soul_of_leah.lua
+++ scripts/furtherance/unlocks/unlocks_leah_b/soul_of_leah.lua
@@ -20,7 +20,7 @@ function SOUL_OF_LEAH:OnMortalDamage(ent, amount, flags, source, countdown)
 			if player:GetCard(i) == SOUL_OF_LEAH.ID then
 				player:UseCard(SOUL_OF_LEAH.ID, UseFlag.USE_OWNED | UseFlag.USE_NOANIM | UseFlag.USE_REMOVEACTIVE)
 				player:RemovePocketItem(i)
-				return {DamageFlags = DamageFlag.DAMAGE_FAKE}
+				return {DamageFlags = flags | DamageFlag.DAMAGE_FAKE}
 			end
 		end
 	end
@@ -33,7 +33,7 @@ Mod:AddPriorityCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, CallbackPriority.LATE, 
 function SOUL_OF_LEAH:OnUse(_, player, flags)
 	player:AddBrokenHearts(2)
 	if not Mod:HasBitFlags(flags, UseFlag.USE_REMOVEACTIVE) then
-		player:TakeDamage(1, DamageFlag.DAMAGE_FAKE, EntityRef(player), 0)
+		player:TakeDamage(1, DamageFlag.DAMAGE_FAKE | DamageFlag.DAMAGE_NO_PENALTIES, EntityRef(player), 0)
 	end
 end
 
